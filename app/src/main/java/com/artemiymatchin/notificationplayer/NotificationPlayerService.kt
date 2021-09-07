e4af13d68491706a4330119759c39bf573c466bb
Artemiy Matchin <art16ws@gmail.com>
2021-09-07 15:55:19 +0300

    Action buttons for notification

--- app/src/main/java/com/artemiymatchin/notificationplayer/NotificationPlayerService.kt
+++ app/src/main/java/com/artemiymatchin/notificationplayer/NotificationPlayerService.kt
@@ -3,26 +3,44 @@ package com.artemiymatchin.notificationplayer
 import android.app.*
 import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
 import android.graphics.Color
 import android.os.Build
 import android.os.IBinder
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
+import androidx.core.graphics.drawable.IconCompat
+import com.artemiymatchin.notificationplayer.actions.LoopActionBroadcastReceiver
 
 class NotificationPlayerService : Service() {
 
     @RequiresApi(Build.VERSION_CODES.O)
-    fun createNotificationChannel(channelId: String, channelName: String): String {
-        val newChannel = NotificationChannel(
+    private fun createNotificationChannel(channelId: String, channelName: String): String {
+        val chan = NotificationChannel(
             channelId,
             channelName, NotificationManager.IMPORTANCE_NONE
         )
-        newChannel.lightColor = Color.BLUE
-        newChannel.lockscreenVisibility = NotificationCompat.VISIBILITY_PRIVATE
+        chan.lightColor = Color.BLUE
+        chan.lockscreenVisibility = NotificationCompat.VISIBILITY_PRIVATE
         val service = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        service.createNotificationChannel(newChannel)
+        service.createNotificationChannel(chan)
         return channelId
+    }
+
+    private fun createAction(
+        receiverClass: Class<*>,
+        icon: Int,
+        title: String?
+    ): NotificationCompat.Action {
+        val broadcastIntent = Intent(application, receiverClass)
+
+        val broadcastPendingIntent: PendingIntent =
+            PendingIntent.getBroadcast(application, 0, broadcastIntent, 0)
+
+        return NotificationCompat.Action.Builder(
+            icon,
+            title,
+            broadcastPendingIntent
+        ).build()
     }
 
     override fun onCreate() {
@@ -35,6 +53,38 @@ class NotificationPlayerService : Service() {
             }
 
 
+        val loopAction = createAction(
+            LoopActionBroadcastReceiver::class.java,
+            R.drawable.repeat_icon,
+            getString(R.string.loop_action)
+        )
+        val pauseAction = createAction(
+            LoopActionBroadcastReceiver::class.java,
+            R.drawable.pause_icon,
+            getString(R.string.pause_action)
+        )
+        val randomAction = createAction(
+            LoopActionBroadcastReceiver::class.java,
+            R.drawable.random_icon,
+            getString(R.string.random_action)
+        )
+
+
+        val notification: Notification = NotificationCompat.Builder(this, channelId)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentTitle(getText(R.string.notification_header))
+            .setSmallIcon(R.drawable.music_icon)
+            .setAutoCancel(true)
+            .addAction(loopAction)
+            .addAction(pauseAction)
+            .addAction(randomAction)
+            .setStyle(
+                androidx.media.app.NotificationCompat.MediaStyle()
+                    .setShowActionsInCompactView(0, 1, 2)
+            )
+            .build()
+
+        startForeground(1, notification)
     }
 
     override fun onBind(intent: Intent?): IBinder? {
--- app/src/main/java/com/artemiymatchin/notificationplayer/actions/LoopActionBroadcastReceiver.kt
+++ app/src/main/java/com/artemiymatchin/notificationplayer/actions/LoopActionBroadcastReceiver.kt
@@ -3,9 +3,10 @@ package com.artemiymatchin.notificationplayer.actions
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 
 class LoopActionBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-
+        Toast.makeText(context, "TEST", Toast.LENGTH_SHORT).show()
     }
 }
