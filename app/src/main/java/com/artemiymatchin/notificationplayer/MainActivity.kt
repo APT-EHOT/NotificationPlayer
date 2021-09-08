c74d47abe53f9ebb4fc07c163744967c9e9e2339
Artemiy Matchin <art16ws@gmail.com>
2021-09-08 19:04:56 +0300

    Added memory permission request

--- app/src/main/java/com/artemiymatchin/notificationplayer/MainActivity.kt
+++ app/src/main/java/com/artemiymatchin/notificationplayer/MainActivity.kt
@@ -1,16 +1,50 @@
 package com.artemiymatchin.notificationplayer
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Intent(this, NotificationPlayerService::class.java).also { intent ->
-            startService(intent)
+        val requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    Intent(this, NotificationPlayerService::class.java).also { intent ->
+                        startService(intent)
+                    }
+                } else {
+                    Toast.makeText(
+                        applicationContext,
+                        getString(R.string.ungiven_permission_msg),
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+
+        when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(
+                application,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) -> {
+                Intent(this, NotificationPlayerService::class.java).also { intent ->
+                    startService(intent)
+                }
+            }
+            else -> {
+                requestPermissionLauncher.launch(
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            }
         }
     }
 }
